69030c4fe614c3b1feade0f1fcb03014250f929d
gaucho1218 <gaucho1218@gmail.com>
2018-09-25 22:29:22 +0900

    #5 WIP

--- include/AIGifParser.h
+++ include/AIGifParser.h
@@ -6,17 +6,6 @@
 
 using namespace std;
 
-enum EGifInfo
-{
-	EGI_HDR = 0,
-	EGI_OFFSET,
-	EGO_SIZE
-};
 
-using TGifInfo = tuple<EGifInfo, int, short>;
-
-//! parse gif data and return its attributes, offset, size
-//! check TGifInfo size part if header type has additional size
-AIDLL TGifInfo ParseGifData(const char *pBuf, const int nSize, const int nOffset) noexcept;
 
 #endif
--- source/AIGifParser.cpp
+++ source/AIGifParser.cpp
@@ -1,6 +1,2 @@
 #include "AIGifParser.h"
 
-TGifInfo ParseGifData(const char *pBuf, const int nSize, const int nOffset) noexcept
-{
-	TGifInfo;
-}
