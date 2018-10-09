e03b5302e6d697a92fb1224411fb7010c6d7f171
gaucho1218 <gaucho1218@gmail.com>
2018-10-09 19:58:56 +0900

    #12 #16 WIP

--- source/AIGifParser.cpp
+++ source/AIGifParser.cpp
@@ -16,11 +16,14 @@ CAiGifParser::~CAiGifParser(void)
 
 int CAiGifParser::ParseGifData(char *pBuf, const int nSize, const int nOffset)
 {
+	if (pBuf == nullptr || nSize <= 0 || nOffset < 0)
+		return -1;
+
 	int nRet = 0;
 
-	if (m_nParserState & AIGIFPARSER_STATE_HDR != AIGIFPARSER_STATE_HDR)
+	if ((m_nParserState & AIGIFPARSER_STATE_HDR) != AIGIFPARSER_STATE_HDR)
 		nRet = ParseHeader(pBuf, nSize, nOffset);
-	else if (m_nParserState & AIGIFPARSER_STATE_LSCRDESC != AIGIFPARSER_STATE_LSCRDESC)
+	else if ((m_nParserState & AIGIFPARSER_STATE_LSCRDESC) != AIGIFPARSER_STATE_LSCRDESC)
 		nRet = ParseLogicalScreenDesc(pBuf, nSize, nOffset);
 
 	return nRet;
@@ -33,19 +36,17 @@ int CAiGifParser::ParseHeader(char *pBuf, const int nSize, const int nOffset)
 	if (memcmp(pBuf, strGif, 3) == 0)
 	{
 		pBuf += 3;
+		nRet = 6;
+		m_nParserState |= AIGIFPARSER_STATE_HDR;
+
 		if (memcmp(pBuf, str87a, 3) == 0)
-		{
 			cout << str87a << endl;
-			m_nParserState |= AIGIFPARSER_STATE_HDR;
-		}
 		else if (memcmp(pBuf, str89a, 3) == 0)
-		{
 			cout << str89a << endl;
-			m_nParserState |= AIGIFPARSER_STATE_HDR;
-		}
 		else
 		{
 			cout << "Unknown GIF Version: ";
+			m_nParserState = AIGIFPARSER_STATE_NONE;
 			cout.write(pBuf, 3) << endl;
 			nRet = -1;
 		}
@@ -58,7 +59,12 @@ int CAiGifParser::ParseLogicalScreenDesc(char *pBuf, const int nSize, const int 
 {
 	int nRet = 0;
 
-
+	SAiGifGlobal *pDesc;
+	if (nSize >= sizeof(SAiGifGlobal))
+	{
+		//! @todo copy
+		pDesc = (SAiGifGlobal *)pBuf;
+	}
 
 	return nRet;
 }
--- win/test_AnImage/main.cpp
+++ win/test_AnImage/main.cpp
@@ -3,23 +3,43 @@
 
 
 #include <stdio.h>
+#include <cstring>
 #include "AIGifParser.h"
+
+#define BUFSIZE	4*1024
 
 int main(int argc, const char * argv[])
 {
 	printf("testing!\n");
 
-	char arrBuf[4096];
+	char arrBuf[BUFSIZE];
 
 	FILE *pFile = nullptr;
 	fopen_s(&pFile, "../../Resource/1.Honeycam.gif", "rb");
 
+	int nOffset = 0;
+	int nReadSize = 0;
+	int nUsed = 0;
+	CAiGifParser tParser;
+
 	if (pFile != nullptr)
 	{
-		int nReadSize = fread(arrBuf, 1, 4096, pFile);
+		while ((nReadSize = fread(arrBuf + nUsed, 1, BUFSIZE - nUsed, pFile)) != 0)
+		{
+			nReadSize += nUsed;
+			nUsed = 0;
 
-		CAiGifParser tParser;
-		tParser.ParseGifData(arrBuf, nReadSize, 0);
+			int nRet = 0;
+
+			nRet = tParser.ParseGifData(arrBuf, nReadSize, nOffset);
+			if (nRet > 0)
+			{
+				nOffset += nRet;
+				nUsed += nRet;
+			}
+
+			memmove(arrBuf, arrBuf + nUsed, BUFSIZ - nUsed);
+		}
 
 
 		fclose(pFile);
