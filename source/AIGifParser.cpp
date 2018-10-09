f0dbffa6a44035c13ab5f8258a814a433201a5c7
gaucho1218 <gaucho1218@gmail.com>
2018-10-09 19:25:50 +0900

    #15 done

--- source/AIGifParser.cpp
+++ source/AIGifParser.cpp
@@ -5,7 +5,8 @@
 #include "AIGifHdr.h"
 
 CAiGifParser::CAiGifParser(void)
-	: m_bGifVarified(false), m_uLogicalWidth(0), m_uLogicalHeight(0)
+	: m_uLogicalWidth(0), m_uLogicalHeight(0),
+	m_nParserState(AIGIFPARSER_STATE_NONE)
 {
 }
 
@@ -17,28 +18,47 @@ int CAiGifParser::ParseGifData(char *pBuf, const int nSize, const int nOffset)
 {
 	int nRet = 0;
 
-	if (m_bGifVarified == false)
+	if (m_nParserState & AIGIFPARSER_STATE_HDR != AIGIFPARSER_STATE_HDR)
+		nRet = ParseHeader(pBuf, nSize, nOffset);
+	else if (m_nParserState & AIGIFPARSER_STATE_LSCRDESC != AIGIFPARSER_STATE_LSCRDESC)
+		nRet = ParseLogicalScreenDesc(pBuf, nSize, nOffset);
+
+	return nRet;
+}
+
+int CAiGifParser::ParseHeader(char *pBuf, const int nSize, const int nOffset)
+{
+	int nRet = 0;
+
+	if (memcmp(pBuf, strGif, 3) == 0)
 	{
-		//! check GIF Signature and Version
-		if (memcmp(pBuf, strGif, 3) == 0)
+		pBuf += 3;
+		if (memcmp(pBuf, str87a, 3) == 0)
 		{
-			pBuf += 3;
-			if (memcmp(pBuf, str87a, 3) == 0)
-				cout << str87a << endl;
-			else if (memcmp(pBuf, str89a, 3) == 0)
-				cout << str89a << endl;
-			else
-			{
-				cout << "Unknown GIF Version: ";
-				cout.write(pBuf, 3) << endl;
-				nRet = -1;
-			}
+			cout << str87a << endl;
+			m_nParserState |= AIGIFPARSER_STATE_HDR;
+		}
+		else if (memcmp(pBuf, str89a, 3) == 0)
+		{
+			cout << str89a << endl;
+			m_nParserState |= AIGIFPARSER_STATE_HDR;
+		}
+		else
+		{
+			cout << "Unknown GIF Version: ";
+			cout.write(pBuf, 3) << endl;
+			nRet = -1;
 		}
 	}
-	else
-	{
 
-	}
+	return nRet;
+}
+
+int CAiGifParser::ParseLogicalScreenDesc(char *pBuf, const int nSize, const int nOffset)
+{
+	int nRet = 0;
+
+
 
 	return nRet;
 }
